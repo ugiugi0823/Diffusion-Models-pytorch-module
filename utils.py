262bad3c14acb2939e8d53eeb370a6dcf58a9bee
Hyunwook Jo <acerghjk@gmail.com>
2023-04-18 16:48:58 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import os
 import torch
-import torchvision
+import torchvision 
 from PIL import Image
 from matplotlib import pyplot as plt
 from torch.utils.data import DataLoader
@@ -39,3 +39,26 @@ def setup_logging(run_name):
     os.makedirs("results", exist_ok=True)
     os.makedirs(os.path.join("models", run_name), exist_ok=True)
     os.makedirs(os.path.join("results", run_name), exist_ok=True)
+
+
+
+import torch
+from torch import nn
+from tqdm.auto import tqdm
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from torchvision.utils import make_grid
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+torch.manual_seed(0) # Set for testing purposes, please do not change!
+
+def show_tensor_images(image_tensor, num_images=25, size=(1, 28, 28)):
+    '''
+    이미지를 시각화하는 기능: 이미지의 텐서, 이미지의 수,
+    이미지당 크기, 균일한 그리드에 이미지를 플롯하고 인쇄합니다.
+    '''
+    image_tensor = (image_tensor + 1) / 2
+    image_unflat = image_tensor.detach().cpu()
+    image_grid = make_grid(image_unflat[:num_images], nrow=5)
+    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+    plt.show()
