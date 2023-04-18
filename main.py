8ee856c4241dfd05c87bf9e18839dff6f62c2a9b
Hyunwook Jo <acerghjk@gmail.com>
2023-04-18 15:26:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     p.add_argument("--batch_size", type=int, default=8, help='(정수)배치 사이즈를 정해주세요')
     p.add_argument("--image_size", type=int, default=64, help='(정수)이미지 사이지를 정해주세요')
     p.add_argument("--num_classes", type=int, default=10, help='(정수)클래스의 갯수를 넣어주세요')
-    p.add_argument("--dataset_path", type=str, default=r"/content/Diffusion-Models-pytorch/datasets/Landscape_classifier_02/training", help='(글자)데이터셋 경로를 넣어주세요')
+    p.add_argument("--dataset_path", type=str, default=r"/content/Diffusion-Models-pytorch-module/datasets/Landscape_classifier_02/training", help='(글자)데이터셋 경로를 넣어주세요')
     p.add_argument("--device", type=str, default="cuda", help='(글자)GPU 로 돌릴지 CPU로 돌릴지 정해주세요!')
     p.add_argument("--lr", type=float, default=3e-4, help='(부동소수점)러닝레이트를 넣어주세요')
                     
